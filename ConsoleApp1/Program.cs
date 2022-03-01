13761bdb90e4bde354f2ec9d8cebb819770f6036
user <user@DESKTOP-K00KJFK>
2022-03-01 22:03:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задание 1
+Console.WriteLine("Hello, World!");
+Console.ReadKey();
+// Задание 2
+Console.Write("Hello");
+Console.Write("World ");
+Console.Write("!!!");
+Console.ReadKey();
+// При использовании Console.Write все что будет вводиться на последующих строках все равно окажеться на одной строке  в проекте.Console.WriteLine выделяет новую строку
